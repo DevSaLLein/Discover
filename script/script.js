0d5e095ee241e7985cc8207cf5b76b36ab0f768b
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-01-07 18:04:57 -0300

    finish base project

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
-const root = document.querySelector("#root");
+const buttonSwitch = document.querySelector("#switch");
+const handleClickButton = () => {
+    const root = document.documentElement;
+
+    if(root.classList.contains('dark')) {
+        root.classList.remove('dark');
+        root.classList.add('light');
+    }
+    else {
+        root.classList.remove('light');
+        root.classList.add('dark');
+    }
+};
+
+buttonSwitch.addEventListener('click', handleClickButton);
